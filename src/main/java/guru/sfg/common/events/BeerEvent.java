6809ca5a782d6d360b998eed6e9fbc60318e9560
reinchard60 <reinchard60@hotmail.com>
2025-09-13 10:28:12 -0400

    implementing New Inventory Listener - closes #8

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,19 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -3469741108493905848L;
+
+    private BeerDto beerDto;
+}
